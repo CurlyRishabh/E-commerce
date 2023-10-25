3072d01fcc3204564d677258a9906c7ea66f0791
CurlyRishabh <rissag940@gmail.com>
2023-10-25 12:58:37 +0530

    added functionality to cart close button

--- client/src/Pages/Cart/cart.jsx
+++ client/src/Pages/Cart/cart.jsx
@@ -16,10 +16,10 @@ function TableQtyCounter(props){
 
 		return (
 			<>
-				<td className>
-					<div className="flex w-30">
+				<td className="">
+					<div className="flex items-center justify-center w-30">
 						<button
-							className="border m-1 w-8 h-8 text-lg rounded-md bg-slate-300 hover:bg-slate-400 font-semibold"
+							className="border w-6 h-6 flex items-center justify-center  text-lg rounded-md bg-slate-300 hover:bg-slate-400 font-semibold"
 							disabled={count <= 0}
 							onClick={() => handleCount(-1)}
 						>
@@ -27,7 +27,7 @@ function TableQtyCounter(props){
 						</button>
 						<p className="p-2 font-semibold">{count}</p>
 						<button
-							className="border m-1 w-8 h-8 text-lg rounded-md bg-slate-300 hover:bg-slate-400 font-semibold"
+							className="border w-6 h-6 flex items-center justify-center  text-lg rounded-md bg-slate-300 hover:bg-slate-400 font-semibold"
 							onClick={() => handleCount(1)}
 						>
 							+
@@ -35,19 +35,27 @@ function TableQtyCounter(props){
 					</div>
 				</td>
 				<td>${count * props.newPrice}</td>
+				<td className="">
+					<button
+						onClick={() => props.handleCloseClick(props.title)}
+						className="relative text-base left-9  w-6  rounded-full text-white bg-rose-500"
+					>
+						x
+					</button>
+				</td>
 			</>
 		);
+		
 }
 
 function TableRow(props){
 	return (
-		<tr className="p3 text-sm text-gray-700 border-2 h-28 align-middle">
+		<tr className="p3  text-xs text-gray-700 border-2 h-20 ">
 			<td>
-				<img src={props.img} className="w-36 p-2" />
+				<img src={props.img} className="w-28 pb-4" />
 			</td>
 			<td>{props.title}</td>
-			<TableQtyCounter countResult={props.countResult} newPrice={props.newPrice} />
-			
+			<TableQtyCounter title={props.title} handleCloseClick={props.handleCloseClick} countResult={props.countResult} newPrice={props.newPrice} />
 		</tr>
 	);
 }
@@ -56,13 +64,24 @@ function Cart() {
 	const [x, setX] = useState([]);
 
 	const [total, setTotal] = useState(Number(data[0].newPrice) + Number(data[3].newPrice) + Number(data[19].newPrice));
-	
+	function handleCloseClick(title) {
+		console.log(title);
+		const temp = x.filter((y) => y.title !== title);
+		console.log(temp);
+		if(temp.length===0)
+			Cookies.set("productList", JSON.stringify([]), {expires: 1});
+		else
+			Cookies.set("productList", JSON.stringify(temp), {expires: 1});
+		setX(temp)
+
+		
+	}
 	useEffect(() => {
 		const cookieValue = Cookies.get("productList");
 
 		if (cookieValue) {
 			const parsedData = JSON.parse(cookieValue);
-			
+			console.log("hello");
 			setX(parsedData);
 		}
 	}, []);
@@ -70,37 +89,39 @@ function Cart() {
 	function countResult(add) {
 		setTotal(total + add);
 	}
-	function handleCount(){
-		
-	}
-	function del(name, val) {
-		const filtered = x.filter((a) => a.title !== name);
-		console.log(filtered);
-		setX(filtered);
-		setTotal(total - val);
-	}
-	function handleCheckOut() {
-		console.log(x);
-	}
+	
 
 	return (
 		<div>
 			<h1>Shopping Cart</h1>
-			<table className="ml-10 w-5/6 border-2 table-auto	">
-				<thead className="border bg-gray-300">
-					<tr className="p-3 text-sm font-semibold tracking-wide text-left">
-						<th >Product</th>
-						<th >Name</th>
-						<th >Qty</th>
-						<th >Price</th>
-					</tr>
-				</thead>
-				<tbody>
-					{x.map((y) => (
-						<TableRow handleCount={handleCount}countResult={countResult} newPrice={y.newPrice} title={y.title} img={y.img} />
-					))}
-				</tbody>
-			</table>
+			<div className="w-full p-2">
+				<table className="  m-3 border-2 w-5/6 text-center	">
+					<thead className="border bg-gray-300 ">
+						<tr className="p-3 text-sm font-semibold ">
+							<th>Product</th>
+							<th>Name</th>
+							<th>Qty</th>
+							<th>
+								Price
+								
+							</th>
+							<th className=""></th>
+						</tr>
+					</thead>
+					<tbody>
+						{x.map((y) => (
+							<TableRow
+								
+								countResult={countResult}
+								handleCloseClick={handleCloseClick}
+								newPrice={y.newPrice}
+								title={y.title}
+								img={y.img}
+							/>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
